c62da3ec8472606677f976039b2e30234a96842c
thaijasa <thaijasa.bv11@gmail.com>
2019-08-27 14:57:21 -0700

    added more weather related information to forecast function

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,7 +2,7 @@ const request = require('request')
 
 
 const forecast = (latitude, longitude, callback) => {
-    const url = "https://api.darksky.net/forecast/21caacf39474cf6e52b6fef5b40663e8/" + latitude + "," + longitude
+    const url = "https://api.darksky.net/forecast/21caacf39474cf6e52b6fef5b40663e8/" + latitude + "," + longitude + "?units=si"
 
     request({url: url, json:true}, (error, response)=>{
         if(error){
@@ -11,7 +11,8 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find location, try another search!", undefined)
         }else{
             callback(undefined, response.body.daily.data[0].summary + "It is currently " + response.body.currently.temperature + " degrees out. There is a " + 
-                    response.body.currently.precipProbability + "% chance of rain.")
+                    response.body.currently.precipProbability + "% chance of rain. The minimum temperature for today is " + response.body.daily.data[0].temperatureMin + 
+                    "deg C and the maximum temperature for today is " + response.body.daily.data[0].temperatureMax + "deg C")
         }
     })
     
